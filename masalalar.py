d1769255db5ec256eae2c24593efaf6fa1a0cce0
Durdiyeva Rayxona <durdiyevarayxona60@gmail.com>
2025-10-08 18:25:08 +0500

    8.10.2025
    my projects

---
+++ masalalar.py
@@ -0,0 +1,71 @@
+##1
+# kun=input("kunni kiriting:")
+# oddiy_kun=47
+# stakan_narxi=22000
+# dam_olish_kuni=["Shanba", "Yakshanba"]
+# if kun in dam_olish_kuni:
+#     stakanlar=oddiy_kun*1.5
+# else:
+#     stakanlar=oddiy_kun
+#
+# daromad=int(stakanlar*stakan_narxi)
+# print(f"{kun.capitalize()} kun daromadi:{daromad}")
+##2
+# steps=int(input("steps:"))
+# step_length=0.7
+# today_step=8200
+# if steps % step_length == 0:
+#     print("ok")
+# else:
+#     print("not ok")
+##3
+# chiroq = 60
+# chiroqlar_soni = 4
+# ishlash_vaqti = 6
+#
+#
+# umumiy_sarf = chiroq * chiroqlar_soni * ishlash_vaqti
+# umumiy_sarf = umumiy_sarf/ 1000
+#
+#
+# print("Dilnozaning kunlik elektr energiyasi sarfi:", umumiy_sarf, "kVt·soat")
+##4
+# un = 450
+# tuxum_dona = 5
+# shakar = 250
+# tort_soni = 3
+# jami_un = un * tort_soni
+# jami_tuxum = tuxum_dona * tort_soni
+# jami_shakar = shakar * tort_soni
+# print("3 ta tort uchun kerak bo‘ladi:")
+# print("Un:", jami_un, "gramm")
+# print("Tuxum:", jami_tuxum, "dona")
+# print("Shakar:", jami_shakar, "gramm")
+##5
+# height = float(input("Bo'yingizni metrlarda kiriting (masalan, 1.75): "))
+# weight = float(input("Vazningizni kilogrammlarda kiriting (masalan, 70): "))
+# bmi = weight / (height * height)
+# print("Sizning BMI ko'rsatkishingiz:", (bmi, 2))
+# if bmi < 18.5:
+#     print("Siz ozg'insiz.")
+# elif bmi < 25:
+#     print("Sizda normal (ideal) vazn.")
+# elif bmi < 30:
+#     print("Sizda ortiqcha vazn bor.")
+# else:
+#     print("Sizda semizlik muammosi bor.")
+##6
+# suv = 1200
+# kunlik_koeff = 0.988
+# for kun in range(10):
+#     suv *= kunlik_koeff
+# print("10 kundan keyin qoladigan suv miqdori:", (suv, 1), "litr")
+##7
+# kunlik_foiz = 12
+# kunlar_soni = (100 / kunlik_foiz)
+# print("Kamolaga kitobni tugatish uchun kerak bo‘lgan kun:", kunlar_soni)
+##8
+
+
+
+
